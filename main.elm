5448cd1979b352850303771d0b1749714556f5ef
Daniel Kremniov <daniel.kremniov@icelolly.com>
2017-07-05 14:20:52 +0100

    Changes

--- main.elm
+++ main.elm
@@ -1,10 +1,11 @@
 module Main exposing (..)
 
+import Dict
 import Html exposing (..)
-import RemoteData
 import Http
 import Json.Decode as Decode
-import Dict
+import Json.Decode.Pipeline as Pipeline
+import RemoteData
 
 
 -- MODEL --
@@ -17,11 +18,14 @@ type alias Model =
 
 
 type alias ProviderUrl =
-    ( String, String )
+    { name : String
+    , url : String
+    }
 
 
 type alias ProviderData =
-    { hotels : List Hotel }
+    { hotels : List Hotel
+    }
 
 
 type alias Provider =
@@ -32,7 +36,16 @@ type alias Provider =
 
 
 type alias Hotel =
-    { name : String }
+    { accomodationId : String
+    , accomodationName : String
+    , deals : List Deal
+    }
+
+
+type alias Deal =
+    { hotel : String
+    , offerId : String
+    }
 
 
 init : ( Model, Cmd Msg )
@@ -49,9 +62,44 @@ view model =
     div []
         [ div []
             [ text "Providers: "
+            , viewStatusBar model.providers
             ]
         , div [] [ viewHotels model.hotels ]
         ]
+
+
+viewStatusBar : Maybe (Dict.Dict String Provider) -> Html Msg
+viewStatusBar providers =
+    let
+        providersSize : Dict.Dict String Provider -> Int
+        providersSize providers =
+            Dict.size providers
+
+        filterFetchedProviders : String -> Provider -> Bool
+        filterFetchedProviders _ provider =
+            case provider.data of
+                RemoteData.Success _ ->
+                    True
+
+                _ ->
+                    False
+
+        fetchedProvidersSize : Dict.Dict String Provider -> Int
+        fetchedProvidersSize providers =
+            Dict.size <|
+                Dict.filter filterFetchedProviders providers
+    in
+    case providers of
+        Just providers_ ->
+            if providersSize providers_ == 0 then
+                text "Loading"
+            else if providersSize providers_ == fetchedProvidersSize providers_ then
+                text "Loaded"
+            else
+                text (toString (fetchedProvidersSize providers_) ++ " of " ++ toString (providersSize providers_))
+
+        Nothing ->
+            text ""
 
 
 viewHotels : RemoteData.WebData (List Hotel) -> Html Msg
@@ -73,7 +121,7 @@ viewHotels hotels =
 viewHotel : Hotel -> Html Msg
 viewHotel hotel =
     div []
-        [ text ("Hotel name: " ++ hotel.name) ]
+        [ text ("Hotel name: " ++ hotel.accomodationName) ]
 
 
 
@@ -91,7 +139,7 @@ subscriptions model =
 
 type Msg
     = ProvidersUrlsFetched (RemoteData.WebData (List ProviderUrl))
-    | ProviderDataFetched String (RemoteData.WebData ProviderData)
+    | ProviderDataFetched String String (RemoteData.WebData ProviderData)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -103,8 +151,7 @@ update msg model =
                 cmds =
                     case response of
                         RemoteData.Success providersUrls ->
-                            List.map (\p -> Tuple.second p) providersUrls
-                                |> List.map fetchProviderData
+                            List.map fetchProviderData providersUrls
 
                         _ ->
                             []
@@ -115,30 +162,47 @@ update msg model =
                         RemoteData.Success providersUrls ->
                             Just <|
                                 Dict.fromList <|
-                                    List.map createProviderTuple providersUrls
+                                    List.map nameProviderTuple providersUrls
 
                         _ ->
                             Nothing
-            in
-                { model | providers = providers }
-                    ! cmds
 
-        ProviderDataFetched url response ->
-            ({ model
+                hotels : RemoteData.WebData (List Hotel)
+                hotels =
+                    case response of
+                        RemoteData.Failure e ->
+                            RemoteData.Failure e
+
+                        _ ->
+                            RemoteData.Loading
+            in
+            { model
+                | providers = providers
+                , hotels = hotels
+            }
+                ! cmds
+
+        ProviderDataFetched name url response ->
+            { model
                 | hotels = updateHotels model.hotels (hotelsFromProviderDataResponse response)
-             }
-            )
+                , providers = updateProvider name (Provider name url response) model.providers
+            }
                 ! []
 
 
-createProviderTuple : ProviderUrl -> ( String, Provider )
-createProviderTuple ( name, url ) =
+updateProvider : String -> Provider -> Maybe (Dict.Dict String Provider) -> Maybe (Dict.Dict String Provider)
+updateProvider name provider providers =
+    case providers of
+        Just providers_ ->
+            Just (Dict.update name (\_ -> Just provider) providers_)
+
+        Nothing ->
+            providers
+
+
+nameProviderTuple : ProviderUrl -> ( String, Provider )
+nameProviderTuple { name, url } =
     ( name, Provider name url RemoteData.Loading )
-
-
-createProviderUrl : String -> String -> ProviderUrl
-createProviderUrl name url =
-    ( name, url )
 
 
 hotelsFromProviderDataResponse : RemoteData.WebData ProviderData -> RemoteData.WebData (List Hotel)
@@ -165,7 +229,7 @@ updateHotels old new =
                 _ ->
                     old
     in
-        RemoteData.map2 mergeHotels mappedOld new
+    RemoteData.map2 mergeHotels mappedOld new
 
 
 mergeHotels : List Hotel -> List Hotel -> List Hotel
@@ -175,14 +239,16 @@ mergeHotels old new =
 
 fetchProvidersUrls : Cmd Msg
 fetchProvidersUrls =
-    Http.get "http://localhost:3000/providersUrls" decodeProvidersUrls
+    Http.get "http://localhost:8080/providersUrls" decodeProvidersUrls
         |> RemoteData.sendRequest
         |> Cmd.map ProvidersUrlsFetched
 
 
 decodeProviderUrl : Decode.Decoder ProviderUrl
 decodeProviderUrl =
-    Decode.map2 createProviderUrl (Decode.field "name" Decode.string) (Decode.field "url" Decode.string)
+    Pipeline.decode ProviderUrl
+        |> Pipeline.required "name" Decode.string
+        |> Pipeline.required "url" Decode.string
 
 
 decodeProvidersUrls : Decode.Decoder (List ProviderUrl)
@@ -190,26 +256,47 @@ decodeProvidersUrls =
     Decode.list decodeProviderUrl
 
 
-fetchProviderData : String -> Cmd Msg
-fetchProviderData url =
+fetchProviderData : ProviderUrl -> Cmd Msg
+fetchProviderData { name, url } =
     Http.get url decodeProviderData
         |> RemoteData.sendRequest
-        |> Cmd.map (ProviderDataFetched url)
+        |> Cmd.map (ProviderDataFetched name url)
 
 
 decodeProviderData : Decode.Decoder ProviderData
 decodeProviderData =
-    Decode.map ProviderData (Decode.field "hotels" decodeHotels)
+    Decode.map ProviderData <|
+        (Decode.field "deals" decodeDeals
+            |> Decode.andThen
+                (\deals ->
+                    Decode.field "hotels" (decodeHotels deals)
+                )
+        )
 
 
-decodeHotel : Decode.Decoder Hotel
-decodeHotel =
-    Decode.map Hotel (Decode.field "name" Decode.string)
+decodeHotel : List Deal -> Decode.Decoder Hotel
+decodeHotel deals =
+    Pipeline.decode Hotel
+        |> Pipeline.required "accomodationId" Decode.string
+        |> Pipeline.required "accomodationName" Decode.string
+        |> Pipeline.hardcoded deals
 
 
-decodeHotels : Decode.Decoder (List Hotel)
-decodeHotels =
-    Decode.list decodeHotel
+decodeHotels : List Deal -> Decode.Decoder (List Hotel)
+decodeHotels deals =
+    Decode.list (decodeHotel deals)
+
+
+decodeDeals : Decode.Decoder (List Deal)
+decodeDeals =
+    Decode.list decodeDeal
+
+
+decodeDeal : Decode.Decoder Deal
+decodeDeal =
+    Pipeline.decode Deal
+        |> Pipeline.required "hotel" Decode.string
+        |> Pipeline.required "offerId" Decode.string
 
 
 
